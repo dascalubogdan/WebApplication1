87567822c4f9acf5d3c30b687cb51d5463b4e811
bogdan dascalu <dascalu.bogdan@yahoo.com>
2017-02-19 18:38:36 +0200

    3

--- WebApplication1.Tests/UnitTest1.cs
+++ WebApplication1.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            //ssssdddsssddd
+            //ssssdddsssdddss
             Assert.AreEqual(5, 2+2);
         }
     }
