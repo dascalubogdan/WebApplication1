98151295e32fe19cdb0e6a23b8d26efc8b37b153
bogdan dascalu <dascalu.bogdan@yahoo.com>
2017-02-19 16:16:06 +0200

    3

---
+++ WebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, 2+2);
+        }
+    }
+}
