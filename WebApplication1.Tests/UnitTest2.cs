9b7e8eda58bbff8d3ac8dd0052f7e208a58adeb1
v-bodasc <v-bodasc@microsoft.com>
2017-03-09 17:44:40 +0200

    5

---
+++ WebApplication1.Tests/UnitTest2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication1.Tests
+{
+    using Controllers;
+
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            HomeController c = new HomeController();
+            Assert.IsNotNull(c.Index());
+        }
+    }
+}
