f0ceb27f34cca24203b3abd33488369e04214329
v-bodasc <v-bodasc@microsoft.com>
2017-03-09 16:49:01 +0200

    2

---
+++ WebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController h = new HomeController();
+        }
+    }
+}
